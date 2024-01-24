b5967a92ba0a759387dd5cb6c8090a6153ba9003
dolamiao <121911373@qq.com>
2024-01-24 15:01:09 +0800

    123

--- js/myjs.js
+++ js/myjs.js
@@ -1,3 +1,7 @@
-success({
-    result: "im result"
-})
+datas=[
+    {title:"qq",url:"www.qq.com"},
+    {title:"163",url:"www.163.com"},
+    {title:"mi",url:"www.mi.com"},
+    {title:"bing",url:"www.bing.com"},
+    {title:"weibo",url:"www.weibo.com"},
+]
