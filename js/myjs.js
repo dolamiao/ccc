bbe674afee4adc4fbd41ea4a159b8e57a8e05f60
dolamiao <121911373@qq.com>
2024-01-24 17:23:07 +0800

    1

--- js/myjs.js
+++ js/myjs.js
@@ -1,66 +1,174 @@
 datas=[
-    {title:"qq",url:"www.qq.com"},
-    {title:"163",url:"www.163.com"},
-    {title:"mi",url:"www.mi.com"},
-    {title:"bing",url:"www.bing.com"},
-    {title:"weibo",url:"www.weibo.com"},
-    {title:"360",url:"www.360.cn"},
-    {title:"163",url:"www.163.com"},
-    {title:"jd",url:"www.jd.com"},
-    {title:"ifeng",url:"www.ifeng.com"},
-    {title:"qidian",url:"www.qidian.com"},
-    {title:"weibo",url:"www.weibo.com"},
-    {title:"weibo",url:"www.weibo.com"},
-    {title:"weibo",url:"www.weibo.com"},
-    {title:"weibo",url:"www.weibo.com"},
-    {title:"weibo",url:"www.weibo.com"},
-    {title:"weibo",url:"www.weibo.com"},
-    {title:"qq",url:"www.qq.com"},
-    {title:"163",url:"www.163.com"},
-    {title:"mi",url:"www.mi.com"},
-    {title:"bing",url:"www.bing.com"},
-    {title:"weibo",url:"www.weibo.com"},
-    {title:"360",url:"www.360.cn"},
-    {title:"163",url:"www.163.com"},
-    {title:"jd",url:"www.jd.com"},
-    {title:"ifeng",url:"www.ifeng.com"},
-    {title:"qidian",url:"www.qidian.com"},
-    {title:"weibo",url:"www.weibo.com"},
-    {title:"weibo",url:"www.weibo.com"},
-    {title:"weibo",url:"www.weibo.com"},
-    {title:"weibo",url:"www.weibo.com"},
-    {title:"weibo",url:"www.weibo.com"},
-    {title:"weibo",url:"www.weibo.com"},
-    {title:"qq",url:"www.qq.com"},
-    {title:"163",url:"www.163.com"},
-    {title:"mi",url:"www.mi.com"},
-    {title:"bing",url:"www.bing.com"},
-    {title:"weibo",url:"www.weibo.com"},
-    {title:"360",url:"www.360.cn"},
-    {title:"163",url:"www.163.com"},
-    {title:"jd",url:"www.jd.com"},
-    {title:"ifeng",url:"www.ifeng.com"},
-    {title:"qidian",url:"www.qidian.com"},
-    {title:"weibo",url:"www.weibo.com"},
-    {title:"weibo",url:"www.weibo.com"},
-    {title:"weibo",url:"www.weibo.com"},
-    {title:"weibo",url:"www.weibo.com"},
-    {title:"weibo",url:"www.weibo.com"},
-    {title:"weibo",url:"www.weibo.com"},
-    {title:"qq",url:"www.qq.com"},
-    {title:"163",url:"www.163.com"},
-    {title:"mi",url:"www.mi.com"},
-    {title:"bing",url:"www.bing.com"},
-    {title:"weibo",url:"www.weibo.com"},
-    {title:"360",url:"www.360.cn"},
-    {title:"163",url:"www.163.com"},
-    {title:"jd",url:"www.jd.com"},
-    {title:"ifeng",url:"www.ifeng.com"},
-    {title:"qidian",url:"www.qidian.com"},
-    {title:"weibo",url:"www.weibo.com"},
-    {title:"weibo",url:"www.weibo.com"},
-    {title:"weibo",url:"www.weibo.com"},
-    {title:"weibo",url:"www.weibo.com"},
-    {title:"weibo",url:"www.weibo.com"},
-    {title:"weibo",url:"www.weibo.com"},
-]
+    {
+      "title": "资料库",
+      "url": "https://sugar.aipage.com/report/r_1013e-7f41qdmb-ol3d46/468f27053ce449eb20d2970508ad6996"
+    },
+    {
+      "title": "供应商面板",
+      "url": "https://sugar.aipage.com/report/r_1013e-6l5oojm-k773j4/d65ae9b806a25f382425e07278d6e202"
+    },
+    {
+      "title": "财务报表",
+      "url": "https://sugar.aipage.com/report/r_1013e-7bq4pmd8-key976/0f547d7aea1611b07ff70ff446f64b30"
+    },
+    {
+      "title": "经营结算",
+      "url": "https://sugar.aipage.com/report/r_1013e-8x6ykxxk-k4qxy6/93b2b00ad2d4675b77049d20fb608c6a"
+    },
+    {
+      "title": "店铺大盘",
+      "url": "https://sugar.aipage.com/dashboard/6acaf767cb2abe07efe013aab3a7ec3e"
+    },
+    {
+      "title": "各渠道聚水潭",
+      "url": "https://sugar.aipage.com/dashboard/058b50145e2fb5b3367b9cd19a279d2f"
+    },
+    {
+      "title": "经营真实报表",
+      "url": "https://sugar.aipage.com/report/r_1013e-2hnd6aq3-kx56ly/1504ca741a32433b45f31527553069bd"
+    },
+    {
+      "title": "报活动专用",
+      "url": "https://sugar.aipage.com/report/r_1013e-alwso1sd-key3wq/26f65b7b892f8b275f0f831cecefba69"
+    },
+    {
+      "title": "店铺分析",
+      "url": "https://sugar.aipage.com/dashboard/e89d0959b59a15dd52d2722b2240189e"
+    },
+    {
+      "title": "行业竞店分析",
+      "url": "https://sugar.aipage.com/report/r_1013e-26nfv93t-kg4zvr/d64003a07eda60a3dd3c4da45130044a"
+    },
+    {
+      "title": "杰毅数据图",
+      "url": "https://sugar.aipage.com/dashboard/3572ed45b1be6708ecff9d0344e50667?conditions=%7B%22multiSelect%22%3A%5B%22C%E5%BA%97%22%5D%2C%22dateRange%22%3A%222023-10-10%2C2023-10-10%22%7D"
+    },
+    {
+      "title": "协同打款",
+      "url": "https://sugar.aipage.com/report/r_1013e-2gbbdmnz-ovq7el/b10d6a19ce20751a19e00d46b6827956"
+    },
+    {
+      "title": "JY款式分析",
+      "url": "https://sugar.aipage.com/report/r_1013e-23tvltmg-1j5qm4/a64de96eb1238a293fca2c188062fcf7"
+    },
+    {
+      "title": "商品分析",
+      "url": "https://sugar.aipage.com/report/r_1013e-bmktezh-k67bw4/2f13f3f3705fa76b4509c47c51c7b4f6"
+    },
+    {
+      "title": "资料维护",
+      "url": "https://sugar.aipage.com/report/r_1013e-9rx5i6mn-key24m/cf877821d001dfac4ed46a3d5bb77189"
+    },
+    {
+      "title": "产品规划",
+      "url": "https://sugar.aipage.com/report/r_1013e-5ljl95d7-oq8w75/bf5dd631663ac66c6962dbdb7388c0b7"
+    },
+    {
+      "title": "产品录入",
+      "url": "https://sugar.aipage.com/report/r_1013e-5lj9t6pk-o5b2jz/9c3d5ed3f585fd024758c393f32a8a72"
+    },
+    {
+      "title": "写景-淘系",
+      "url": "https://sugar.aipage.com/report/r_1013e-idy3m45-ol3qdp/05cd71a51860a8578b71d4e433261789"
+    },
+    {
+      "title": "写景-抖音",
+      "url": "https://sugar.aipage.com/report/r_1013e-7kqxywow-km3zn7/549f2938f21c80213844e5d3cab57f3a"
+    },
+    {
+      "title": "查景-摄影",
+      "url": "https://sugar.aipage.com/report/r_1013e-5rwl0r8u-kwe973/f87e0550cf42d97e5b404ec52453f067"
+    },
+    {
+      "title": "销售排行榜",
+      "url": "https://sugar.aipage.com/report/r_1013e-8oiqxqgr-o5bwpp/86d0692ab846d106cdc52260279bc185"
+    },
+    {
+      "title": "销售排行榜",
+      "url": "https://sugar.aipage.com/report/r_1013e-60a0csov-k4qxyq/b1630c9e5a463962f96c9f291c4f45be"
+    },
+    {
+      "title": "订单管理",
+      "url": "https://sugar.aipage.com/report/r_1013e-g8tvdi2-oyn9bz/f5a87bc51f08214df9049c6c92e067f6"
+    },
+    {
+      "title": "采购计划",
+      "url": "https://sugar.aipage.com/report/r_1013e-96wjtr4n-127r8m/b96fdbb22d2600b9f01d75062065640b"
+    },
+    {
+      "title": "采购分析",
+      "url": "https://sugar.aipage.com/report/r_1013e-13yoc9gc-k77e6b/a1d94ce4e51420593559bcc12b3bc6b0"
+    },
+    {
+      "title": "库存监控",
+      "url": "https://sugar.aipage.com/report/r_1013e-51lapvox-1d7jv2/a9d2b9d6dfcdde9a92533139020b322b"
+    },
+    {
+      "title": "采购入库",
+      "url": "https://sugar.aipage.com/report/r_1013e-426piel4-k6734g/ed84db0e4513a07f91105f7448ac6dfc"
+    },
+    {
+      "title": "下架处理",
+      "url": "https://sugar.aipage.com/report/r_1013e-b2gyphfi-k4qx46/57dd8f53b0846ec99cc04cdec5b34ec7"
+    },
+    {
+      "title": "库存进程",
+      "url": "https://sugar.aipage.com/report/r_1013e-bqeopmy1-1d7y4v/abd96c259b1ac43442563a25893ee3cf"
+    },
+    {
+      "title": "抖音指标",
+      "url": "https://sugar.aipage.com/report/r_1013e-3i5ubfva-o879pm/423a22078f3fcccf7e83327bff65f907"
+    },
+    {
+      "title": "客服指标",
+      "url": "https://sugar.aipage.com/report/r_1013e-9yw8nvku-kx5xrr/b42c76e9a3cf5ff5d32df7253d730008"
+    },
+    {
+      "title": "平台指标",
+      "url": "https://sugar.aipage.com/report/r_1013e-1fyim558-k6734w/ddb10029ae5733bfd0fdf660c08c8a65"
+    },
+    {
+      "title": "售后",
+      "url": "https://sugar.aipage.com/report/r_1013e-4ktx43n7-o5qng9/dc7cf6d047e9a6b8fe890a3d347be174"
+    },
+    {
+      "title": "23年款效",
+      "url": "https://sugar.aipage.com/report/r_1013e-9s50k56g-ol34eb/5154502273f94f8465aea86506756898"
+    },
+    {
+      "title": "达人信息库",
+      "url": "https://sugar.aipage.com/report/r_1013e-ary4xndw-kb7937/f2808c72cde59047724a5a52270cb960"
+    },
+    {
+      "title": "仪表盘",
+      "url": "https://sugar.aipage.com/report/r_1013e-2zls7s1r-kb7dbg/db9b494c136feaf527e59a37ea0f0441"
+    },
+    {
+      "title": "售后",
+      "url": "https://sugar.aipage.com/report/r_1013e-6f3d7v87-ke69b7/3d6ed9f9f6d46375205e750907ea57ac"
+    },
+    {
+      "title": "采购入库",
+      "url": "https://sugar.aipage.com/report/r_1013e-aibwncjh-ovqbgn/c6d4fc166189bd7ddb61bcd0002ed2dc"
+    },
+    {
+      "title": "款式拆解",
+      "url": "https://sugar.aipage.com/report/r_1013e-5cc3c8xo-kg4jl5/3649f59b5b48d1ee96062fa33644e1f2"
+    },
+    {
+      "title": "试穿登记",
+      "url": "https://sugar.aipage.com/report/r_1013e-4ttscdis-km3jzw/a91b7c45f5a17110ed89f29263f2e1fe"
+    },
+    {
+      "title": "试穿分析",
+      "url": "https://sugar.aipage.com/report/r_1013e-51k805e-kzdqb9/13ede4d74fdaee145bacbb54a4a0b5e7"
+    },
+    {
+      "title": "生产排单分析",
+      "url": "https://sugar.aipage.com/report/r_1013e-5opnrd2c-13q2d8/e898d0d6a104741ee284fb6609b3b0de"
+    },
+    {
+      "title": "买鞋登记",
+      "url": "https://sugar.aipage.com/report/r_1013e-xfm2bix-km3jlv/5cd28a8bc024dde140abf5f84b6cf502"
+    }
+  ]
